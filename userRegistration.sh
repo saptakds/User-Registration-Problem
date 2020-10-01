8771fe5ba26f6cfd2e580d547f8430b13f89ceba
saptakds <saptakds@gmail.com>
2020-10-01 16:58:52 +0530

    Added: Lastname starts with Cap and has min 3 chars

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,7 +4,16 @@ read -p "Enter first name: " firstName
 patFname="^([A-Z]*[a-z]){3}$"
 if [[ $firstName =~ $patFname ]]
 then
-	echo "Valid Firstname"
+	echo "Valid firstname"
 else
 	echo "Invalid firstname"
+fi
+#Last Name validation
+read -p "Enter last name: " lastName
+patLname="^([A-Z]*[a-z]){3}$"
+if [[ $lastName =~ $patLname ]]
+then
+    echo "Valid lastname"
+else
+    echo "Invalid lastname"
 fi
